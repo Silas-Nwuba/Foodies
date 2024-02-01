beadaec4a6518f0219772c3b3dea83b76ac6bcd0
Silas-Nwuba <nwubasilas@yahoo.com>
2024-02-01 19:47:21 +0100

    cleaning up the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,31 @@
-import React, { Suspense, lazy } from "react";
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import FocusLock from "react-focus-lock";
 import { BookedContext } from "../context/BookedContext";
-import SpinnerFullPage from "../components/SpinnerFullPage";
-const HomePage = lazy(() => import("../pages/HomePage"));
-const RecipePage = lazy(() => import("../pages/RecipePage"));
-const SearchPage = lazy(() => import("../pages/SearchPage"));
-const CategoryPage = lazy(() => import("../pages/CategoryPage"));
-const ErrorMessage = lazy(() => import("../components/ErrorMessage"));
+import HomePage from "../pages/HomePage";
+import RecipePage from "../pages/RecipePage";
+import SearchPage from "../pages/SearchPage";
+import CategoryPage from "../pages/CategoryPage";
+import ErrorMessage from "../components/ErrorMessage";
 const App = () => {
   return (
     <BookedContext>
       <BrowserRouter>
-        <FocusLock>
-          <Suspense fallback={<SpinnerFullPage />}>
-            <Routes>
-              <Route index element={<HomePage />}></Route>
-              <Route path="recipe/:idMeal" element={<RecipePage />}>
-                <Route
-                  path="*"
-                  element={<ErrorMessage message="Not Found" />}
-                ></Route>
-              </Route>
-              <Route path="search" element={<SearchPage />}></Route>
-              <Route
-                path="recipe/:idMeal/search"
-                element={<SearchPage />}
-              ></Route>
-              <Route path="category/:name" element={<CategoryPage />}></Route>
-              <Route
-                path="*"
-                element={<ErrorMessage message="Not Found" />}
-              ></Route>
-            </Routes>
-          </Suspense>
-        </FocusLock>
+        <Routes>
+          <Route index element={<HomePage />}></Route>
+          <Route path="recipe/:idMeal" element={<RecipePage />}>
+            <Route
+              path="*"
+              element={<ErrorMessage message="Not Found" />}
+            ></Route>
+          </Route>
+          <Route path="search" element={<SearchPage />}></Route>
+          <Route path="recipe/:idMeal/search" element={<SearchPage />}></Route>
+          <Route path="category/:name" element={<CategoryPage />}></Route>
+          <Route
+            path="*"
+            element={<ErrorMessage message="Not Found" />}
+          ></Route>
+        </Routes>
       </BrowserRouter>
     </BookedContext>
   );
