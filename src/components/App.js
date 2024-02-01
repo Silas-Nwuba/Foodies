2518a47cac278f9303eb96317deb740df206dd5b
Silas-Nwuba <nwubasilas@yahoo.com>
2024-02-01 11:00:57 +0100

    optimizing the bundle

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,40 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "../pages/HomePage";
-import RecipePage from "../pages/RecipePage";
-import ErrorMessage from "./ErrorMessage";
-import SearchPage from "../pages/SearchPage";
+import FocusLock from "react-focus-lock";
 import { BookedContext } from "../context/BookedContext";
-import CategoryPage from "../pages/CategoryPage";
+import SpinnerFullPage from "../components/SpinnerFullPage";
+const HomePage = lazy(() => import("../pages/HomePage"));
+const RecipePage = lazy(() => import("../pages/RecipePage"));
+const SearchPage = lazy(() => import("../pages/SearchPage"));
+const CategoryPage = lazy(() => import("../pages/CategoryPage"));
+const ErrorMessage = lazy(() => import("../components/ErrorMessage"));
 const App = () => {
   return (
     <BookedContext>
       <BrowserRouter>
-        <Routes>
-          <Route index element={<HomePage />}></Route>
-          <Route path="recipe/:idMeal" element={<RecipePage />}>
-            <Route
-              path="*"
-              element={<ErrorMessage message="Not Found" />}
-            ></Route>
-          </Route>
-          <Route path="search" element={<SearchPage />}></Route>
-          <Route path="recipe/:idMeal/search" element={<SearchPage />}></Route>
-          <Route path="category/:name" element={<CategoryPage />}></Route>
-          <Route
-            path="*"
-            element={<ErrorMessage message="Not Found" />}
-          ></Route>
-        </Routes>
+        <FocusLock>
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route index element={<HomePage />}></Route>
+              <Route path="recipe/:idMeal" element={<RecipePage />}>
+                <Route
+                  path="*"
+                  element={<ErrorMessage message="Not Found" />}
+                ></Route>
+              </Route>
+              <Route path="search" element={<SearchPage />}></Route>
+              <Route
+                path="recipe/:idMeal/search"
+                element={<SearchPage />}
+              ></Route>
+              <Route path="category/:name" element={<CategoryPage />}></Route>
+              <Route
+                path="*"
+                element={<ErrorMessage message="Not Found" />}
+              ></Route>
+            </Routes>
+          </Suspense>
+        </FocusLock>
       </BrowserRouter>
     </BookedContext>
   );
---
+++ src/components/SpinnerFullPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "../style/SpinnerFullPage.module.css";
+
+const SpinnerFullPage = () => {
+  return <div className={`${style.spinner}`}></div>;
+};
+
+export default SpinnerFullPage;
